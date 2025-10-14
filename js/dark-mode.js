22b208d1e09ee9775657ae5478e21b0eb815f474
Anabelen <tu@email.comatorreslanda@gm>
2025-10-13 18:02:14 -0600

    dm 2

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,1 +1,6 @@
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
+/**
+ * este es el boton de cambio de tema
+ */click:
+toggleThemeBtn. addEventListener(' click', function () {
+    alert ('me diste click');
