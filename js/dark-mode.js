a18b7028b755e6e9050c5875a74f2358556adfeb
Anabelen <tu@email.comatorreslanda@gm>
2025-10-15 09:49:27 -0600

    dm5

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -2,9 +2,12 @@ const themeToggleBtn = document.getElementById("theme-toggle-btn");
 themeToggleBtn.addEventListener("click", function() {
     document.body.classList.toggle("dark");
     if (document.body.classList.contains("dark")) {
-        themeToggleBtn.innerText = "Light Mode";
+        themeToggleBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        
+
     } else {
+        themeToggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
         //si el body no tiene la clase "dark" se cambia el texto del boton  a "Dark Mode"
-        themeToggleBtn.innerText = "Dark Mode";
+        //themeToggleBtn.innerText = "Dark Mode";
     }
 }) ;
